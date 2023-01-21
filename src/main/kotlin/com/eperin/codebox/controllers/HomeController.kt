bcbc1cfddf34a0843e040dbd1497620b3435e1bf
Eric Perin <eric.perin@outlook.com>
2023-01-20 21:47:07 -0300

    add home controller

---
+++ src/main/kotlin/com/eperin/codebox/controllers/HomeController.kt
@@ -0,0 +1,12 @@
+package com.eperin.codebox.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+@RestController
+class HomeController {
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String? {
+        return String.format("Hello %s!", name)
+    }
+}
