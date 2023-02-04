3bcc37b195147111162d6b1cb5e228042a44be64
Eric Perin <eric.perin@outlook.com>
2023-02-04 12:55:44 -0300

    remove traceId

--- src/main/kotlin/com/eperin/codebox/configurations/InterceptorConfig.kt
+++ src/main/kotlin/com/eperin/codebox/configurations/InterceptorConfig.kt
@@ -2,21 +2,19 @@ package com.eperin.codebox.configurations
 
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.MDC
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
-import java.util.UUID
 
 @Component
 class InterceptorConfig : HandlerInterceptor {
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        MDC.put("requestId", UUID.randomUUID().toString())
+//        MDC.put("requestId", UUID.randomUUID().toString())
         return true
     }
 
     override fun afterCompletion(
         request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?
     ) {
-        MDC.remove("requestId")
+//        MDC.remove("requestId")
     }
 }
